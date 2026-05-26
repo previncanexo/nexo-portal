9813ef1bb3ebae37852e60572062719e34447b80
Mattbreak <matias@breakmkt.com.ar>
2026-05-26 13:47:13 -0300

    feat: agregar Meta Pixel 2187028668714954

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { DM_Sans, DM_Serif_Display } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { InstallBanner } from './components/InstallBanner'
 
@@ -85,6 +86,23 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           {children}
           <InstallBanner />
         </div>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2187028668714954');
+          fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=2187028668714954&ev=PageView&noscript=1" alt="" />
+        </noscript>
       </body>
     </html>
   )
